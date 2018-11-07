04a8713e82f152b90a010b1e8c7b2031ecfc94c2
Amos <musicrock95@hotmail.com>
2018-11-08 01:42:46 +0800

    Update on addmovie kotlin file
    Toast list

--- app/src/main/java/com/example/amoslim_164934f/movierater/AddMovie.kt
+++ app/src/main/java/com/example/amoslim_164934f/movierater/AddMovie.kt
@@ -3,6 +3,7 @@ package com.example.amoslim_164934f.movierater
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_movie.*
 
 class AddMovie : AppCompatActivity() {
@@ -12,8 +13,8 @@ class AddMovie : AppCompatActivity() {
         setContentView(R.layout.activity_add_movie)
 
 
-        chb1audi.setOnClickListener(View.OnClickListener {
-            if(chb1audi.isChecked == true)
+        chbaudi.setOnClickListener(View.OnClickListener {
+            if(chbaudi.isChecked == true)
             {
                 chklinear.setVisibility(View.VISIBLE)
             }
@@ -29,9 +30,65 @@ class AddMovie : AppCompatActivity() {
     fun onMyBtnClick(v: View){
 
 
+        var titlename = txtname.text.toString()
+        var desc = txtdesc.text.toString()
+        var resdate = txtdaterelease.text.toString()
+        var laugauge = ""
+        var audichecked =""
+        var reason = ""
 
-        var date = daterelease.text.toString()
 
+        if (rbtneng.isChecked == true) {
+            laugauge = "English"
+        }
+        else if (rbtnchi.isChecked == true){
+            laugauge = "Chinese"
+        }
+        else if (rbtnmalay.isChecked == true){
+            laugauge = "Malay"
+        }
+        else if (rbtntamil.isChecked == true){
+            laugauge = "Tamil"
+        }else
+            laugauge = "null"
+
+
+        if (chbaudi.isChecked == false) {
+            audichecked = "true"
+        }
+        else {
+            audichecked = "false"
+        }
+
+
+        if(chb3lang.isChecked == true)
+        {
+            reason="Langauge"
+        }
+        else
+        {
+            reason=""
+        }
+
+
+        if (chb2vio.isChecked == true)
+        {
+            reason="Violence"
+        }
+        else
+            reason =""
+
+
+
+
+        Toast.makeText(getApplicationContext(), "Title = ${titlename}"
+                + "\n" + "Overview = ${desc}"
+                + "\n" + "Release date = ${resdate}"
+                + "\n" + "Lauguage = ${laugauge}"
+                + "\n" + "Suitable for ages = ${audichecked}"
+                + "\n" + "Reason: ${reason}"
+
+            ,Toast.LENGTH_SHORT).show()
 
     }
 }
