4c595359f82c4597c66fea79ef48cb79950ebd93
Amos <musicrock95@hotmail.com>
2018-11-13 01:58:06 +0800

    simplify codes

--- app/src/main/java/com/example/amoslim_164934f/movierater/AddMovie.kt
+++ app/src/main/java/com/example/amoslim_164934f/movierater/AddMovie.kt
@@ -1,6 +1,5 @@
 package com.example.amoslim_164934f.movierater
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,16 +14,16 @@ class AddMovie : AppCompatActivity() {
 
 
     // Hide or show checkbox when selected not suitable for all audience checkbox
-        chbaudi.setOnClickListener {
-            if(chbaudi.isChecked == true)
+        chbaudi.setOnClickListener{
+            if(chbaudi.isChecked)
             {
-                chklinear.setVisibility(View.VISIBLE)
+                chklinear.visibility = View.VISIBLE
             }
             else
             {
-                chklinear.setVisibility(View.INVISIBLE)
+                chklinear.visibility = View.INVISIBLE
                 chblang.isChecked= false
-                chbvio.isChecked=false
+                chbvio.isChecked= false
             }
         }
 
@@ -42,34 +41,22 @@ class AddMovie : AppCompatActivity() {
         var reasonVio = ""
         var reasonLang = ""
         var NoReason = ""
-        rbtneng.isChecked == true
+        rbtneng.isChecked
 
         //vaildation if field/textbox is empty
-        if (titlename.length == 0) {
-            txtname.setError("Field empty, enter movie title");
-
-        } else if (desc.length == 0) {
-            txtdesc.setError("Field empty, Enter Description");
-
-        } else if (resdate.length == 0) {
-            txtdaterelease.setError("Field empty");
-
-        } else {
+        if (titlename.isEmpty()) txtname.error = "Field empty, enter movie title"
+        else if (desc.isEmpty()) txtdesc.error = "Field empty, Enter Description"
+        else if (resdate.isEmpty()) txtdaterelease.error = "Field empty"
+        else {
 
 
             // for laugauge checkbox
-            if (rbtneng.isChecked == true) {
-                laugauge = "English"
-            }
-            else if (rbtnchi.isChecked == true) {
-                laugauge = "Chinese"
-            }
-            else if (rbtnmalay.isChecked == true) {
-                laugauge = "Malay"
-            }
-            else if (rbtntamil.isChecked == true) {
-                laugauge = "Tamil"
-            }
+            if (rbtneng.isChecked == true) laugauge = "English"
+            else if (rbtnchi.isChecked == true) laugauge = "Chinese"
+            else if (rbtnmalay.isChecked == true) laugauge = "Malay"
+            else if (rbtntamil.isChecked == true) laugauge = "Tamil"
+
+            // Show Toast Display
 
             //not suitable for audience radio button
             if (chblang.isChecked == true) {
@@ -84,18 +71,16 @@ class AddMovie : AppCompatActivity() {
                 reasonVio = ""
             }
 
-
-            if (chbaudi.isChecked == true) {
+            if (chbaudi.isChecked) {
                 audichecked = "true"
-            }
-            else {
+            } else {
                 audichecked = "false"
                 NoReason = "null"
 
             }
 
             // Show Toast Display
-            Toast.makeText(getApplicationContext(), "Title = ${titlename}"
+            Toast.makeText(applicationContext, "Title = ${titlename}"
                     + "\n" + "Overview = ${desc}"
                     + "\n" + "Release date = ${resdate}"
                     + "\n" + "Lauguage = ${laugauge}"
